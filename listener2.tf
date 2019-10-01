a647f7604c8b6f8ba9b5d4f91cb5720f4fb6623e
Chris.Morgan <chris.morgan@clearcloud.co.uk>
2019-10-01 12:41:38 +0000

    removing instance attachmenT

--- listener2.tf
+++ listener2.tf
@@ -36,12 +36,3 @@ resource "aws_lb_target_group" "l2_alb_target_group" {
   }
 }
 
-# -------------------
-# Listener2 - Target Group Attachment
-# -------------------
-resource "aws_lb_target_group_attachment" "l2_target_group" {
-  count            = "${length(split(",", var.listener2_target_id))}"
-  target_group_arn = "${aws_lb_target_group.l2_alb_target_group.arn}"
-  target_id        = "${var.listener2_target_id}"
-  port             = "${var.listener2_svc_port}"
-}
