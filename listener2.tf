01d762f47828f8cb39408eb8455afb1d5b3c4b2d
Chris.Morgan <chris.morgan@clearcloud.co.uk>
2019-10-01 08:17:58 +0000

    targets just as var

--- listener2.tf
+++ listener2.tf
@@ -41,6 +41,6 @@ resource "aws_lb_target_group" "l2_alb_target_group" {
 resource "aws_lb_target_group_attachment" "l2_target_group" {
   count            = "${length(split(",", var.listener2_target_id))}"
   target_group_arn = "${aws_lb_target_group.l2_alb_target_group.arn}"
-  target_id        = "${element(split(",",var.listener2_target_id), count.index)}"
+  target_id        = "${var.listener2_target_id}"
   port             = "${var.listener2_svc_port}"
 }
